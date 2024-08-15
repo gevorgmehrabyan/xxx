7bb5cc6b32154f46fa69ceed3c9b7532db40186d
gevorgmehrabyan <mehrabyangevorg@gmail.com>
2024-08-15 19:27:37 +0700

    TEST ARGUMENTS

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,44 @@
 import React from 'react';
-import { Document, Page, Text } from '@react-pdf/renderer';
+import { Document, Page, Text, PDFViewer } from '@react-pdf/renderer';
 
-const createPDFDocument = (data: { text: string }, formType: string): JSX.Element => {
-    switch (formType) {
+export const createPDFDocument = (data: { text: string }, options: {formType: string, withPreview: boolean}): JSX.Element => {
+    switch (options?.formType) {
         case 'FORM1': {
-            return (
+            return options?.withPreview ? <PDFViewer>
                 <Document>
                     <Page>
-                        <Text>Form 1</Text>
+                        <Text>Form 1 With Preview</Text>
+                        <Text>{data?.text}</Text>
+                    </Page>
+                </Document>
+            </PDFViewer>  : (
+                <Document>
+                    <Page>
+                        <Text>Form 1 Without Preview</Text>
                         <Text>{data?.text}</Text>
                     </Page>
                 </Document>
             );
         }
+
+        case 'FORM2': {
+            return options?.withPreview ? <PDFViewer>
+                <Document>
+                    <Page>
+                        <Text>Form 2 With Preview</Text>
+                        <Text>{data?.text}</Text>
+                    </Page>
+                </Document>
+            </PDFViewer>  : (
+                <Document>
+                    <Page>
+                        <Text>Form 2 Without Preview</Text>
+                        <Text>{data?.text}</Text>
+                    </Page>
+                </Document>
+            );
+        }
+
         default:
             return (
                 <Document>
@@ -23,5 +49,3 @@ const createPDFDocument = (data: { text: string }, formType: string): JSX.Elemen
             );
     }
 };
-
-export default createPDFDocument;
