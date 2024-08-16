ad59c29b87ba0f33b0e9e7f7482d1b94d7613e39
gevorgmehrabyan <mehrabyangevorg@gmail.com>
2024-08-16 12:39:50 +0700

    fonts configs

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,22 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import { Document, Page, Text, renderToFile, Font } from '@react-pdf/renderer';
 import SentToLawyer from './SentToLawyer';
-import path from 'path';
 Font.register({
     family: 'Times New Roman',
     fonts: [
         {
-            src: path.join(__dirname, 'fonts/timesNewRomanBold.ttf'),
+            src: 'src/fonts/timesNewRomanBold.ttf',
             fontWeight: 'bold',
         },
         {
-            src: path.join(__dirname, 'fonts/timesNewRoman.ttf'),
+            src: 'src/fonts/timesNewRoman.ttf',
         },
         {
-            src: path.join(__dirname, 'fonts/timesNewRomaItalic.ttf'),
+            src: 'src/fonts/timesNewRomaItalic.ttf',
             fontStyle: 'italic',
         },
         {
-            src: path.join(__dirname, 'fonts/timesNewRomanBoldItalic.ttf'),
+            src: 'src/fonts/timesNewRomanBoldItalic.ttf',
             fontStyle: 'italic',
             fontWeight: 'bold',
         },
@@ -27,10 +26,10 @@ Font.register({
     family: 'Arial MT',
     fonts: [
         {
-            src: path.join(__dirname, 'fonts/arialMT.ttf'),
+            src: 'src/fonts/arialMT.ttf',
         },
         {
-            src: path.join(__dirname, 'fonts/ArialMTBoldItalic.ttf'),
+            src: 'src/fonts/ArialMTBoldItalic.ttf',
             fontStyle: 'italic',
             fontWeight: 'bold',
         },
