c4851b474b70176c01171a59940085d0bf8b03f8
gevorgmehrabyan <mehrabyangevorg@gmail.com>
2024-08-16 11:53:58 +0700

    Testing PDF View

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,43 @@
 import React from 'react';
 import { Document, Page, Text, renderToFile, Font } from '@react-pdf/renderer';
 import SentToLawyer from './SentToLawyer'
+import path from 'path';
 
 Font.register({
     family: 'Times New Roman',
     fonts: [
         {
-            src: './fonts/timesNewRomanBold.ttf',
+            src: path.join(__dirname, 'fonts/timesNewRomanBold.ttf'),
             fontWeight: 'bold',
         },
         {
-            src: './fonts/timesNewRoman.ttf',
+            src: path.join(__dirname, 'fonts/timesNewRoman.ttf'),
         },
         {
-            src: './fonts/timesNewRomaItalic.ttf',
+            src: path.join(__dirname, 'fonts/timesNewRomaItalic.ttf'),
             fontStyle: 'italic',
         },
         {
-            src: './fonts/timesNewRomanBoldItalic.ttf',
+            src: path.join(__dirname, 'fonts/timesNewRomanBoldItalic.ttf'),
             fontStyle: 'italic',
             fontWeight: 'bold',
         },
     ],
-})
+});
 
 Font.register({
     family: 'Arial MT',
     fonts: [
         {
-            src: './fonts/arialMT.ttf',
+            src: path.join(__dirname, 'fonts/arialMT.ttf'),
         },
         {
-            src: './fonts/ArialMTBoldItalic.ttf',
+            src: path.join(__dirname, 'fonts/ArialMTBoldItalic.ttf'),
             fontStyle: 'italic',
             fontWeight: 'bold',
         },
     ],
-})
+});
 
 const createPDFDocument = (data: { text: string }, options: {formType: string, withPreview: boolean}): JSX.Element => {
     switch (options?.formType) {
